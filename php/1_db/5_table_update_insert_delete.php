744f7f5fcb0f15b655858ab4860f4238b1962a11
Jaroslaw-Uzarowicz <uzarowicz84@gmail.com>
2022-11-09 07:53:31 +0100

    XD

--- php/1_db/5_table_update_insert_delete.php
+++ php/1_db/5_table_update_insert_delete.php
@@ -13,7 +13,7 @@
   <!-- komunikaty -->
     <?php
       if (isset($_SESSION['info'])) {
-        echo $_SESSION['infoszx'];
+        echo $_SESSION['info'];
         unset($_SESSION['info']);
       }
     ?>
@@ -63,6 +63,26 @@ ADDUSER;
 
       if(!empty($_GET['updateuserid'])) {
         echo "<h4>Aktualizacja użytkownika o id=$_GET[updateuserid]</h4>";
+        $sql="SELCET * FROM `users` WHERE `id` =$_GET[updateuserid]";
+        $result=$conn->query($sql);
+        $user= $result ->fetch_assoc();
+        echo <<< UPDATEUSER
+          <form action="./scripts/add_user.php" method="post">
+            <select name="city_id">
+UPDATEUSER;
+        $sql="SELECT * FROM `cities`";
+        $result=$conn->query($sql);
+        while ($city=$result->fetch_assoc()) {
+          echo "<option value=\"$city[id]\">$city[city]</option>";
+        }
+        echo <<< UPDATEUSER
+            </select><br><br>
+            <input type="text" name="name" placeholder="Podaj imię"><br><br>
+            <input type="text" name="surname" placeholder="Podaj nazwisko"><br><br>
+            <input type="submit" value="Dodaj użytkownika"><br><br>
+          </form>
+UPDATEUSER;
+
       }
      ?>
 
