aebbc461c2c55eefdcd0b800a2636ddf530236b1
Jaroslaw-Uzarowicz <uzarowicz84@gmail.com>
2022-11-09 10:39:31 +0100

    ESSA

---
+++ python/6_2_math_random.py
@@ -0,0 +1,15 @@
+import math as m
+import random as r
+
+# pi
+print(m.pi)
+
+# pierwiastek
+print(m.sqrt(9))
+
+# losowanie
+
+print(r.random())
+
+list = r.choice([1, 2, 3])
+print(list)
---
+++ python/7_slowniki.py
@@ -0,0 +1,8 @@
+slownik = {'klucz1': 'wartość1', 'klucz2': 'wartość2'}
+print(slownik)
+print(slownik['klucz1'])
+
+#utwórz słownik o nazwie worker zawierający klucze: imie , nazwsiko, miasto, wiek, imiona_dzieci i imiona_rodziców
+
+worker={'imie': 'Eryk','nazwsiko': 'Szymanski','miasto': 'Poznan','wiek': '888888','imiona_dzieci': ['Er', 'Yk'],'imiona_rodzicow': ['Szy','Manski']}
+print(worker)
