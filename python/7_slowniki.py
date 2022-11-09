56328b83163d87ac862e3e945ceb945653581a6c
Jaroslaw-Uzarowicz <uzarowicz84@gmail.com>
2022-11-09 11:12:16 +0100

    XD

--- python/7_slowniki.py
+++ python/7_slowniki.py
@@ -2,7 +2,27 @@ slownik = {'klucz1': 'wartość1', 'klucz2': 'wartość2'}
 print(slownik)
 print(slownik['klucz1'])
 
-#utwórz słownik o nazwie worker zawierający klucze: imie , nazwsiko, miasto, wiek, imiona_dzieci i imiona_rodziców
+# utwórz słownik o nazwie worker zawierający klucze: imie , nazwsiko, miasto, wiek, imiona_dzieci i imiona_rodziców
 
-worker={'imie': 'Eryk','nazwsiko': 'Szymanski','miasto': 'Poznan','wiek': '888888','imiona_dzieci': ['Er', 'Yk'],'imiona_rodzicow': ['Szy','Manski']}
+worker = {'imie': 'Eryk', 'nazwsiko': 'Szymanski', 'miasto': 'Poznan',
+          'wiek': '888888', 'imiona_dzieci': ['Er', 'Yk'], 'imiona_rodzicow': ['Szy', 'Manski']}
 print(worker)
+print(worker['imiona_rodzicow'])
+print(worker['imiona_rodzicow'][0])
+
+
+worker['wzrost'] = 197
+print(worker)
+
+key = 'imie'
+
+if key in worker:
+    del worker[key]
+    print(f'Klucz {key} został usunięty!')
+else:
+    print(f'Klucz {key} nie został usunięty')
+
+print(worker)
+
+# dokończyć słowniki
+
---
+++ python/ex_totolotek.py
@@ -0,0 +1,8 @@
+# utwórz program totolotek, użytkownik podaje 6 liczb
+
+import random as r
+
+print('Podaj sześć liczb w zakresie od 1 do 49: ')
+
+nums = []
+
